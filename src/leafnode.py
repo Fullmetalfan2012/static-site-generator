c75e807c5244fe918df6fb8268e7351e1a82cd3b
fullmetalfan2012 <fullmetalfan2012@gmail.com>
2026-03-01 03:37:16 +0100

    Fixed void stuff

--- src/leafnode.py
+++ src/leafnode.py
@@ -9,6 +9,13 @@ class LeafNode(HTMLNode):
             raise ValueError("LeafNode value cannot be None")
         if not self.tag:
             return self.value
+
+        # Void elements cannot have closing tags or content
+        void_elements = {"img", "br", "hr", "input", "meta", "link", "area", "base", "col", "embed", "param", "source", "track", "wbr"}
+
+        if self.tag in void_elements:
+            return f"<{self.tag}{self.props_to_html()}>"
+
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
     
     def __repr__(self):
--- src/textnode.py
+++ src/textnode.py
@@ -36,6 +36,6 @@ def text_node_to_html_node(text_node):
         case TextType.LINK:
             return LeafNode("a", text_node.text, {"href": text_node.url})
         case TextType.IMAGE:
-            return LeafNode("img", "image", {"src": text_node.url, "alt": text_node.text})
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
         case _:
             raise ValueError(f"Unknown TextType: {text_node.text_type}")
